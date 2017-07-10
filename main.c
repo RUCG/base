23658e150cdd97d5caaad6b3924b55de715ddb5f
RUCG <gian-andrea@gmx.ch>
2017-07-10 15:42:08 +0200

    Edited main.c chanched Makefile

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
 #include <avr/io.h>
 
+#define LED1_AUS PORTD &= ~(1<<PD1);//ein Bit wird gelöscht
+#define LED1_EIN PORTD |= ~(1<<PD1);// Ein Bit auf eins
+
 int main (void)
 {
+
+DDRD = 0xFF; //Port D als Ausgang setzen
+PORTD = 0xFF;// alle Pins an Port D auf 1
+
+LED1_EIN;
 
 	while(1)
 	{
