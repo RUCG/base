f5117dcc127541bce52da11b65c27a888c23c60c
RUCG <gian-andrea@gmx.ch>
2017-08-08 14:09:06 +0200

    funktion Tastereinlesen erstellt

--- main.c
+++ main.c
@@ -1,161 +1,52 @@
 #include <stdlib.h>
 #include <avr/io.h>
 
+#define TASTER1 !(PINB & (1<<PB1))// Wenn TASTER1 auf 0
+#define LED_ON (PORTD |= (1<<PD0))// Befehl um die LED zum leuchten bringen
+#define LED_OFF (PORTD &= ~(1<<PD0))// Befehl um LED aus zu schalten
 
-#define F_CPU 8000000
-#define LED1_AUS PORTD &= ~(1<<PD0)
-#define LED1_EIN PORTD |= (1<<PD0)
 
-int main ()
+short taster(char nr)
 {
-
-	while(1)// Das Licht wird zu einem Lauflicht
+	if(nr == 1)
 	{
+		if(TASTER1)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return 0;	
+}
+
 	
-		long i = 0;// Der Speichertyp der Tackte (Normal 0 Tackte)
-		long o = 0;// Der Speichertyp für den zeiten Tackt
+	
 
-		DDRD = 0xFF; //Port D als Ausgang setzen
-		PORTD = 0;// alle Pins an Port D ansprechen
-
-		DDRB &= ~(1<<PB1);
-		PORTB |=(1<<PB1);
-		
-		DDRB &= ~(1<<PB2);
-		PORTB |=(1<<PB2);// PORT befehl für die Pause
-
-		
-
-		while(LICHT_STOPP)// während des drücken des Tasters
-		{
-			PORTD = 1;
-		}
-			while(LANGSAM)
-			{
-				for(o=0; o<1600000; o++)
-				{
-				PORTD = 1;
-				}
-			}	
-		for(i=0; i<27; i++)
-		{
-			PORTD = 1;
-		}
-
-		
-		while(LICHT_STOPP)
-		{
-			PORTD = 2;
-		}
-			while(LANGSAM)
-			{
-				for(o=0; o<1600000; o++)
-				{
-				PORTD = 2;
-				}
-			}	
-		for(i=0; i<27; i++) //Pause für s leuchten
-		{
-			PORTD = 2;
-		}
-
-
-		while(LICHT_STOPP)
-		{
-			PORTD = 4;
-		}
-			while(LANGSAM)
-			{
-				for(o=0; o<1600000; o++)
-				{
-				PORTD = 4;
-				}
-			}	
-		for(i=0; i<27; i++) 
-		{
-			PORTD = 4;
-		}
-		
-
-		while(LICHT_STOPP)
-		{
-			PORTD = 8;
-		}
-			while(LANGSAM)
-			{
-				for(o=0; o<1600000; o++)
-				{
-				PORTD = 8;
-				}
-			}
-		for(i=0; i<27; i++) //Die Zeit ist doppelt so lang um eine optische Täuschung hervor zu rufen
-		{
-			PORTD = 8;
-		}
-
-
-		while(LICHT_STOPP)
-		{
-			PORTD = 16;
-		}
-			while(LANGSAM)
-			{
-				for(o=0; o<1600000; o++)
-				{
-					PORTD = 16;
-				}
-			}
-		for(i=0; i<27; i++) 
-		{
-			PORTD = 16;
-		}
-
-
-		while(LICHT_STOPP)
-		{
-			PORTD = 32;
-		}
-			while(LANGSAM)
-			{
-				for(o=0; o<1600000; o++)
-				{
-					PORTD = 32;
-				}
-			}
-		for(i=0; i<27; i++) 
-		{
-			PORTD = 32;
-		}
-
-		PORTD = 0;
-		
-	}	
-			
-	return 0;
-}/*int main(void)
-
+int main(void)
 {
-	unsigned long i = 0;
-	unsigned short k = 0;
 	
-	DDRD = 0xFF;
-	PORTD = 0x00;
 	
-		do
+	DDRD = 0xFF; // PORT D wird als Ausgang definiert
+	PORTD = 0x00; //LED grundsäzlich dunkel
+	
+	DDRB = 0x00; // PORTB als Eingang definiert
+	PORTB = 0xFF; // Interner Pull-up gesetzt
+
+	while(1)
+	{
+		if(taster(1)==1)
 		{
+			LED_ON;
+		}
+		else
+		{
+			LED_OFF;
+		}
 	
-			for(i=0; i<=530000; i++)
-			{
-					LED1_EIN;
-			}
-			
-			for(i=0; i<=530000; i++)
-			{
-					LED1_AUS;
-			}
-		k++;
-		}while(k <= 30);
+	}
 	
-	
-	return 0;
+return 0;
 }
